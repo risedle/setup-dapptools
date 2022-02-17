cbbbaaab774d2aa17ecedae6d8c47550f1f14472
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-02-17 12:12:56 +0700

    ✦ Use prebuilt binary

--- install-dapptools.sh
+++ install-dapptools.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -l
 
-nix-env -iA dapp -f https://github.com/dapphub/dapptools/archive/refs/tags/hevm/0.49.0.tar.gz
-nix-env -iA hevm -f https://github.com/dapphub/dapptools/archive/refs/tags/hevm/0.49.0.tar.gz
-nix-env -iA seth -f https://github.com/dapphub/dapptools/archive/refs/tags/hevm/0.49.0.tar.gz
-
+curl https://dapp.tools/install | sh
